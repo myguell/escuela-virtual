106cc40d2951100dc6c3a47060d4ebaf5b87c726
myguell <51376211+myguell@users.noreply.github.com>
2020-10-13 09:26:33 -0400

    Add files via upload

---
+++ tareas/jquery -ajax/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(init);
+
+function init(){
+    $("#mi_libro").booklet(
+        {
+            width:800,
+            height:600,
+
+            closed: true,
+            covers: true,
+            autoCenter: true,
+
+            pagePadding: 0,
+            hoverWidth: 100,
+            
+            
+        }
+    );
+
+    $("#bt-go").click(function(){
+        $("#mi_libro").booklet("gotopage",
+ $("#in_go").val());  
+    });
+}
